bf7deae45ea541c0b46ec3e088d9e86d9639ab50
Shoaib Ahmed <2010.ahmed.shoaib@gmail.com>
2022-10-12 03:53:45 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const http = require('http');
 const requestListener = function (req, res) {
     res.writeHead(200);
     res.end('Hello, World!');
-  }
-  const server = http.createServer(requestListener);
+};
+
+const server = http.createServer(requestListener);
+  
